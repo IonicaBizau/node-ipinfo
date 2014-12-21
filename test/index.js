39f3338e87d2e0772fd1688b98800e089c863296
Ionică Bizău <bizauionica@yahoo.com>
2014-12-21 13:23:32 +0200

    More tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,17 @@
 // Dependencies
 var IpInfo = require("../lib");
 
+// Current ip information
 IpInfo(function (err, cLoc) {
     console.log(err || cLoc);
+
+    // Get information about a known ip
+    IpInfo("8.8.8.8", function (err, cLoc) {
+        console.log(err || cLoc);
+
+        // Get organization
+        IpInfo("8.8.8.8/org", function (err, cLoc) {
+            console.log(err || cLoc);
+        });
+    });
 });
